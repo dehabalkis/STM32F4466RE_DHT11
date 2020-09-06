cb8898a5dd0edbb2096cd353a5e715f4289125b4
Deha Balkis <dehabalkis@gmail.com>
2020-09-06 18:46:47 +0200

    first commit

---
+++ Inc/dht11.h
@@ -0,0 +1,24 @@
+/*
+ *
+ * dht11.h
+ *
+ *
+ * Author: Mehmet Deha Balkis
+ *
+ *
+ */
+
+#ifdef dht_h_
+#define dht_h_
+
+typedef struct
+{
+	float Temp;
+	float Hum;
+}dht11_DataTypedef;
+
+void
+
+#endif /* INC_DHT_H_ */
+
+
